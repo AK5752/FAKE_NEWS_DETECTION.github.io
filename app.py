0b4370231390ec651ac1f438053b8e2b21f7380e
Arjun Kargathara <114335922+AK5752@users.noreply.github.com>
2022-12-22 18:46:28 +0530

    This is my fake news detection project

---
+++ app.py
@@ -0,0 +1,32 @@
+import pickle
+
+from flask import Flask, escape, render_template, request
+
+vector = pickle.load(open("vectorizer.pkl", 'rb'))
+model = pickle.load(open("finalized_model.pkl", 'rb'))
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+@app.route('/prediction', methods=['GET', 'POST'])
+def prediction():
+    if request.method == "POST":
+        news = str(request.form['news'])
+        print(news)
+
+        predict = model.predict(vector.transform([news]))[0]
+        print(predict)
+
+        return render_template("prediction.html", prediction_text="News headline is :  {}".format(predict))
+
+
+    else:
+        return render_template("prediction.html")
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
